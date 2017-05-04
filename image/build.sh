0f9b78cad57e258a7b7143757554f4781cb485c1
Camden Narzt <c.narzt@me.com>
2017-05-04 15:26:09 -0600

    Fix yum repos for EOL status
    The repo configs need to use the baseurl option instead of mirrorlist.
    The url is vault.centos.org not mirror.centos.org for EOL OSs.
    The path format for vault repos is different, rewrite it to work.
    The 64 bit image has a libselinux repo the the 32 bit image does not, support both setups.

--- image/build.sh
+++ image/build.sh
@@ -69,6 +69,12 @@ for VARIANT in $VARIANTS; do
 done
 
 header "Updating system"
+run sed -i.bak -re 's/^(mirrorlist)/#\1/g' -e 's/^#(baseurl)/\1/g' -e 's/mirror(\.centos)/vault\1/g' -e 's|centos/\$releasever/([^/]+)/([^/]+)|5.11/\1/\2|g' /etc/yum.repos.d/CentOS-Base.repo
+rm /etc/yum.repos.d/CentOS-Base.repo.bak
+if [ -f /etc/yum.repos.d/libselinux.repo ] ; then
+run sed -i.bak -re 's/^(mirrorlist)/#\1/g' -e 's/^#(baseurl)/\1/g' -e 's/mirror(\.centos)/vault\1/g' -e 's|centos/\$releasever/([^/]+)/([^/]+)|5.11/\1/\2|g' /etc/yum.repos.d/libselinux.repo
+rm /etc/yum.repos.d/libselinux.repo.bak
+fi
 run yum update -y
 run yum install -y curl epel-release
 
