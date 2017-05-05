50617f50879be1d80bd926b720f7eb241a5891e1
Hongli Lai (Phusion) <hongli@phusion.nl>
2017-05-05 11:50:41 +0200

    Fix indenting

--- image/build.sh
+++ image/build.sh
@@ -71,9 +71,9 @@ done
 header "Updating system"
 run sed -i.bak -re 's/^(mirrorlist)/#\1/g' -e 's/^#(baseurl)/\1/g' -e 's/mirror(\.centos)/vault\1/g' -e 's|centos/\$releasever/([^/]+)/([^/]+)|5.11/\1/\2|g' /etc/yum.repos.d/CentOS-Base.repo
 rm /etc/yum.repos.d/CentOS-Base.repo.bak
-if [ -f /etc/yum.repos.d/libselinux.repo ] ; then
-run sed -i.bak -re 's/^(mirrorlist)/#\1/g' -e 's/^#(baseurl)/\1/g' -e 's/mirror(\.centos)/vault\1/g' -e 's|centos/\$releasever/([^/]+)/([^/]+)|5.11/\1/\2|g' /etc/yum.repos.d/libselinux.repo
-rm /etc/yum.repos.d/libselinux.repo.bak
+if [[ -f /etc/yum.repos.d/libselinux.repo ]]; then
+	run sed -i.bak -re 's/^(mirrorlist)/#\1/g' -e 's/^#(baseurl)/\1/g' -e 's/mirror(\.centos)/vault\1/g' -e 's|centos/\$releasever/([^/]+)/([^/]+)|5.11/\1/\2|g' /etc/yum.repos.d/libselinux.repo
+	rm /etc/yum.repos.d/libselinux.repo.bak
 fi
 run yum update -y
 run yum install -y curl epel-release
