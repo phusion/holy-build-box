405ec8a9aa190b91c5933dc8eb08a66259aaa6ff
Ryan Mast <nightlark@users.noreply.github.com>
2020-02-28 22:12:47 -0800

    Use libstdc++ 7.3.0 with devtoolset-7

--- image/build.sh
+++ image/build.sh
@@ -89,6 +89,8 @@ if [ `uname -m` != x86_64 ]; then
 curl -s https://packagecloud.io/install/repositories/phusion/centos-6-scl-i386/script.rpm.sh | bash
 sed -i 's|$arch|i686|; s|\$basearch|i386|g' $CHROOT/etc/yum.repos.d/phusion*.repo
 DEVTOOLSET_VER=7
+# a 32-bit version of devtoolset-8 would need to get compiled
+GCC_LIBSTDCXX_VERSION=7.3.0
 else
 run yum install -y centos-release-scl
 DEVTOOLSET_VER=8
