07942c4acfb83f46d236cf046e51b1e3e031ed27
Hongli Lai <honglilai@gmail.com>
2021-01-15 19:46:55 +0100

    Increase CI and testing speed by disabling optimizations where appropriate
    Only build with optimizations during a release build, or when building the exe_gc_hardened
    variant. The latter is always built with optimizations because Fortify Source requires so.

--- image/functions.sh
+++ image/functions.sh
@@ -47,3 +47,25 @@ function eval_bool()
 	local VAL="$1"
 	[[ "$VAL" = 1 || "$VAL" = true || "$VAL" = yes || "$VAL" = y ]]
 }
+
+function set_default_cflags()
+{
+	# shellcheck disable=SC2030
+	CFLAGS=$(adjust_optimization_level "-O2")
+	CXXFLAGS="$CFLAGS"
+	export CFLAGS
+	export CXXFLAGS
+}
+
+# Given a string containing compiler flags, adjusts optimization level flags according
+# to global settings.
+function adjust_optimization_level()
+{
+	local VAL="$1"
+	if eval_bool "$DISABLE_OPTIMIZATIONS"; then
+		# shellcheck disable=SC2001
+		sed 's|-O[0-9]*||g' <<<"$VAL"
+	else
+		echo "$VAL"
+	fi
+}
