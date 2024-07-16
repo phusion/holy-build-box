c27d703d7d0b1b3a9c51d1ca5f6e2087a738457a
Hongli Lai <hongli@hongli.nl>
2024-07-16 17:02:03 +0200

    Revert exe variant use of -fPIC

--- image/variants/exe.sh
+++ image/variants/exe.sh
@@ -3,8 +3,8 @@
 # shellcheck source=image/activate_func.sh
 source /hbb/activate_func.sh
 activate_holy_build_box /hbb_exe \
-	"-fPIC" \
-	"-fPIC -static-libstdc++" \
-	"-fPIC" \
-	"-fPIC" \
-	"-fPIC -static-libstdc++"
+	"" \
+	"-static-libstdc++" \
+	"" \
+	"" \
+	"-static-libstdc++"
